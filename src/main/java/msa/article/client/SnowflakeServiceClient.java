e5af30755295491361f963e19e253a3249333a65
park geonhee <dcloyal1@gmail.com>
2025-07-08 11:27:39 +0900

    add controller & env

--- src/main/java/msa/article/client/SnowflakeServiceClient.java
+++ src/main/java/msa/article/client/SnowflakeServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "snowflake-service", url = "http://localhost")
+@FeignClient(name = "snowflake-service", url = "${snowflake-service.url}")
 public interface SnowflakeServiceClient {
 
     @GetMapping("/snowflake")
